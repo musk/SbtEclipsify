8d298d63b1617a31949b165137953afd6a3bce44
Stefan Langer <mailtolanger@googlemail.com>
2010-01-09 22:28:30 +0100

    Changed build to include licenses in jar

--- project/build/SbtEclipsifyProject.scala
+++ project/build/SbtEclipsifyProject.scala
@@ -3,6 +3,16 @@ import de.element34.sbteclipsify._
 
 class SbtEclipsifyPluginProject(info: ProjectInfo) extends PluginProject(info) { // with SbtEclipsifyPlugin {
   override def compileOptions =  super.compileOptions ++ (Unchecked :: Deprecation :: Nil)
+  override def mainResources = super.mainResources +++ "NOTICE" +++ "LICENSE" +++ (path("licenses") * "*")
 
   lazy val scalaTest = "org.scalatest" % "scalatest" % "1.0"
+//  lazy val packageLicenses = task {
+//	  log.info("Creating LICENSE texts...")
+//
+//	  if(licensePath.exists) FileUtilities.clean(licensePath, log)
+//	  FileUtilities.copyDirectory("licenses", licensePath, log)
+//	  FileUtilities.copyFile("LICENSE", this.asInstanceOf[MavenStyleScalaPaths].mainResourcesPath / "LICENSE", log)
+//  }
+//
+//  override def packageAction = super.packageAction dependsOn { packageLicenses }
 }
