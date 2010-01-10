16c8b7f38b8ffaccb20049843cfd7b93e278a38f
Stefan Langer <mailtolanger@googlemail.com>
2010-01-10 13:40:13 +0100

    Added license text and doc to files
    Added license text to each scala file
    Added scaladoc comments to all relevant types and methods.
    Updated README.md for new release

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # SbtEclipsify
 
-Plugin for sbt (http://code.google.com/p/simple-build-tool/) for creating .classpath and .project files for the Ecipse IDE (http://www.eclipse.org). 
+Plugin for sbt (http://code.google.com/p/simple-build-tool/) for creating .classpath and .project files for the Ecipse IDE (http://www.eclipse.org).
 It is currently in Beta state and is not feature complete.
 
 ## License
 Just like sbt this software is distributed under the BSD License (http://www.opensource.org/licenses/bsd-license.php).
 
 ## Getting the Plugin
-In order to use the plugin you either have to download the provided jar or you need to build it yourself (sorry no public release to a repo yet :( )
+In order to use the plugin you can either download one of the provided jars in the dist folder or you can build it yourself.
+Easiest is to add a dependency to the plugin in your plugin defintion `"de.element34" % "sbt-eclipsify" % "<version>"`.
+Calling `update` in sbt will pull in the needed dependency.
+For setting up the plugin see "Using the Plugin in your own project" below.
 
 ### Downloading
 You can find ready made jars in the dist folder on git. Download it and use sbt local dependency feature in order to install it in your project.
@@ -17,22 +20,22 @@ If you have scala sources in your project you need to have the Scala Eclipse plu
 This plugin is currently only compiled against Scala 2.7.7 and there is no crossbuild in place as of yet.
 
 ### Building
-SbtEclipsify uses oh wonder sbt as the build tool. 
-After downloading it from git start sbt and do a publish-local. This should compile all sources, run all tests and install the most recent version of the plugin to your local repo. 
-From now on you can simply use `"de.element34" % "sbteclipsify" % "<buildversion>"` to get the plugin for your own projects.
+SbtEclipsify uses oh wonder sbt as the build tool.
+After downloading it from git start sbt and do a publish-local. This should compile all sources, run all tests and install the most recent version of the plugin to your local repo.
+From now on you can simply use `"de.element34" % "sbt-eclipsify" % "<buildversion>"` to get the plugin for your own projects.
 
 ## Supported properties
 The following properties can be added to the build.properties of your project in order to control the output of the plugin
-`sbt.dependency`(default: false) => if set to true puts the sbt jar on the classpath of the project this is needed if you are creating actions or plugins for sbt 
+`sbt.dependency`(default: false) => if set to true puts the sbt jar on the classpath of the project this is needed if you are creating actions or plugins for sbt
 
 `project.description`(default: Projectname + Projectversion) => Set this to the text used to describe your project. this is directly transfered to the .projectfile's project description tag.
 
-`include.project`(default: false) => if set to true the path to the project definition is added as a source folder to the classpath. 
+`include.project`(default: false) => if set to true the path to the project definition is added as a source folder to the classpath.
 
 `include.plugin`(default: true) => if set to true the path to the plugin defintion is added as a source folder to the classpath.
 
 ## Using the Plugin in your own project
-You need to create a plugin definition in your sbt project. 
+You need to create a plugin definition in your sbt project.
 The plugin definition can be any Scala class that extends PluginDefinition and is located in the plugins directory in the projects subfolder of your sbt project.
 
 Example:
@@ -46,10 +49,10 @@ next create a file name MySbtProjectPlugins.scala and add the following text to 
      import sbt._
 
      class MySbtProjectPlugins(info: ProjectInfo) extends PluginDefinition(info) {
-      	   lazy val eclipse = "de.element34" % "sbteclipsify" % "0.2.0"
+      	   lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.2.0"
      }
 
-This will enable your project to get the plugin in order to use it you need to add it to your project defintion. 
+This will enable your project to get the plugin in order to use it you need to add it to your project defintion.
 Open your project definition file, most likley "MySbtProject.scala" in "project/build/" folder, and add the SbtEclipsifyPlugin trait.
 
     import sbt._
@@ -64,16 +67,13 @@ After reloading the project you should have a new action named "eclipse" which w
 Now all you need to do is import the Project into your Eclipse workspace as an existing Project and everything should work.
 
 ## Known Issues
-The plugin only supports one outputpath currently. Test cannot be compiled to a different path. 
-Crossbuilds are not supported.			 
+The plugin only supports one outputpath currently. Test cannot be compiled to a different path.
+Crossbuilds are not supported.
 
 ## FUTURE
-* Make the project buildable from a simple download
 * Better documentation (as always :) )
 * Add ability to fine tune generation
 * Add ability to include directories besides those in test and main
 * Add generation of subprojects like with eclipse:eclipse from maven
-* Improve the test coverage (still need to figure out how to mock the sbt parts) 
+* Improve the test coverage (still need to figure out how to mock the sbt parts)
 * Add sources when available to classepathentries
-
-
--- src/main/scala/de/element34/sbteclipsify/ClasspathFile.scala
+++ src/main/scala/de/element34/sbteclipsify/ClasspathFile.scala
@@ -1,3 +1,31 @@
+/**
+ * Copyright (c) 2010, Stefan Langer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Element34 nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS ROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.element34.sbteclipsify
 
 import scala.xml._
@@ -10,14 +38,27 @@ abstract class Filter(pattern: String) {
   def mkString = "=\"" + pattern + "\""
 }
 
+/**
+ * Defines a include pattern for a classpath entry. <br />
+ * e.g.: <code>&lt;classpathentry kind="src" path="src/" including="*.scala" /&gt;</code>
+ */
 case class IncludeFilter(pattern: String) extends Filter(pattern) {
   override def mkString = " including" + super.mkString
 }
+/**
+ * Defines a exclude pattern for a classpath entry. <br />
+ * e.g.: <code>&lt;classpathentry kind="src" path="src/" excluding="*.scala" /&gt;</code>
+ */
 case class ExcludeFilter(pattern: String) extends Filter(pattern) {
   override def mkString = " excluding" + super.mkString
 }
-
+/**
+ * Combines <code>IncludeFilter</code> and <code>ExcludeFilter</code> for use in a <code>ClasspathEntry</code>
+ */
 case class FilterChain(inc: Option[IncludeFilter], ex: Option[ExcludeFilter]) {
+  /**
+   * Generates the actual markup for a classpathentry
+   */
   def mkString: String = {
     def getStrOrEmpty[A <: Filter](opt: Option[A]) = {
       opt.map(_.mkString).getOrElse("")
@@ -26,27 +67,53 @@ case class FilterChain(inc: Option[IncludeFilter], ex: Option[ExcludeFilter]) {
   }
 }
 
+/**
+ * Companion object for <code>FilterChain</code> to provide convenience method for their creation.
+ */
 object FilterChain {
   def apply(inc: IncludeFilter, ex: ExcludeFilter) = new FilterChain(Some(inc), Some(ex))
   def apply(inc: IncludeFilter) = new FilterChain(Some(inc), None)
   def apply(ex: ExcludeFilter) = new FilterChain(None, Some(ex))
 }
 
+/**
+ * Special type designating a empty <code>FilterChain</code>
+ */
 object EmptyFilter extends FilterChain(None, None)
 
 abstract class Kind(val name: String)
+/** defines the variable kind("var") for a classpathentry*/
 case object Variable extends Kind("var")
+/** defines the container kind ("con") for a classpathentry */
 case object Container extends Kind("con")
+/** defines the output kind ("output") for a classpathentry */
 case object Output extends Kind("output")
+/** defines the source kind ("src") for a classpathentry */
 case object Source extends Kind("src")
+/** defines the library kind ("lib") for a classpathentry */
 case object Library extends Kind("lib")
 
 object ClasspathConversions {
-  implicit def pathToString(path: Path): String = path.toString
+	/** implicit conversion from a path to a string */
+	implicit def pathToString(path: Path): String = path.toString
 }
 
+/**
+ * Defines a classpathentry in a .classpath file.
+ * Each entry has a kind (either src, output, lib, var or con),
+ * a path designating its location, a optional source path and
+ * a include and exlucde filter as well as arbitrary attributes
+ * that specify further information for the classpathentry.
+ *
+ * @see the eclipse documentatin for further information about classpathentries
+ */
 case class ClasspathEntry(kind: Kind, path: String, srcpath: Option[String], filter: FilterChain, attributes: List[Tuple2[String, String]]) {
+  /** @see mkString(sep: String) */
   def mkString: String = mkString("")
+  /**
+   * converts this <code>ClasspathEntry</code > into a xml string representation
+   * @param sep Defines the leading separater <code>String</code> prepended to each classpathentry
+   */
   def mkString(sep: String): String = {
     sep +
     "<classpathentry kind=\"" + kind.name + "\"" +
@@ -64,7 +131,7 @@ case class ClasspathEntry(kind: Kind, path: String, srcpath: Option[String], fil
 	    }
     )
   }
-
+  /** returns the sourcepath as a string when specified */
   def writeSrcPath(srcpath: Option[String]): String = {
     srcpath match {
       case Some(text) => " sourcepath=\"" +  text + "\""
@@ -73,6 +140,9 @@ case class ClasspathEntry(kind: Kind, path: String, srcpath: Option[String], fil
   }
 }
 
+/**
+ * Factory providing convenience methods for creating <code>ClasspathEntry</code>
+ */
 object ClasspathEntry {
   def apply(kind: Kind, path: String) = new ClasspathEntry(kind, path, None, EmptyFilter, Nil)
   def apply(kind: Kind, path: String, srcpath: String) = new ClasspathEntry(kind, path, Some(srcpath), EmptyFilter, Nil)
@@ -84,102 +154,132 @@ object ClasspathEntry {
   def apply(kind: Kind, path: String, srcpath: String, filter: FilterChain, attributes: List[Tuple2[String, String]]) = new ClasspathEntry(kind, path, Some(srcpath), filter, attributes)
 }
 
+/**
+ * Gathers the structural information for a .classpath file.
+ * @param project The sbt project for which the .classpath file is created
+ * @param log The logger from the sbt project
+ */
 class ClasspathFile(project: Project, log: Logger) {
 	import ClasspathConversions._
     lazy val classpathFile: File = project.info.projectPath / ".classpath" asFile
 
-  def writeFile: Option[String] = {
+    /**
+     * writes the .classpath file to the project root
+     * @return <code>Some(error)</code>, where error designates the error message to display, when an error occures else returns <code>None</code>
+     */
+    def writeFile: Option[String] = {
+    	/**
+    	 * replaces the current content of the .classpath file
+    	 * @return <code>Some(error)</code> when error occures else returns <code>None</code>
+         */
+    	def createOrReplaceWith(content: String): Option[String]= {
+    		FileUtilities.touch(classpathFile, log) match {
+    			case Some(error) =>
+    				Some("Unable to write classpath file " + classpathFile + ": " + error)
+    			case None =>
+    				FileUtilities.write(classpathFile, classpathContent.toString, forName("UTF-8"), log)
+    		}
+    	}
 
-    def createOrReplaceWith(content: String): Option[String]= {
+        /**
+         * @return <code>List[ClasspathEntry]</code> containing entries for each jar contained in path.
+         */
+    	def getDependencyEntries(path: Path): List[ClasspathEntry] = {
+    		import Path._
+    		val files = path.asFile.listFiles
+    		val jars = files.filter(file => file.isFile && file.getName.endsWith(".jar")).map(file => {
+    			val relativePath = Path.relativize(project.info.projectPath, file) match {
+			  		case Some(rPath) => rPath
+			  		case None => Path.fromFile(file)
+    			}
+    			ClasspathEntry(Library, relativePath)
+    		}).toList
+    		val subDirs = files.filter(file => file.isDirectory && file.getName != ".." && file.getName != ".").flatMap(file => getDependencyEntries(Path.fromFile(file))).toList
+    		jars ++ subDirs
+    	}
 
-      FileUtilities.touch(classpathFile, log) match {
-	case Some(error) =>
-	  Some("Unable to write classpath file " + classpathFile + ": " + error)
-	case None =>
-	  FileUtilities.write(classpathFile, classpathContent.toString, forName("UTF-8"), log)
-        }
-    }
+    	val basicScalaPaths = project.asInstanceOf[BasicScalaPaths]
+    	val dependencies = basicScalaPaths.dependencyPath
+    	val managedDependencies = basicScalaPaths.managedDependencyPath
 
-    def getDependencyEntries(path: Path): List[ClasspathEntry] = {
-      import Path._
-      val files = path.asFile.listFiles
-      val jars = files.filter(file => file.isFile && file.getName.endsWith(".jar")).map(file => {
-		  val relativePath = Path.relativize(project.info.projectPath, file) match {
-			  case Some(rPath) => rPath
-			  case None => Path.fromFile(file)
-		  }
-		  ClasspathEntry(Library, relativePath)
-      }).toList
-      val subDirs = files.filter(file => file.isDirectory && file.getName != ".." && file.getName != ".").flatMap(file => getDependencyEntries(Path.fromFile(file))).toList
-      jars ++ subDirs
-    }
+    	val scalaContainer = "ch.epfl.lamp.sdt.launching.SCALA_CONTAINER"
+    	val javaContainer = "org.eclipse.jdt.launching.JRE_CONTAINER"
 
-    val basicScalaPaths = project.asInstanceOf[BasicScalaPaths]
-    val dependencies = basicScalaPaths.dependencyPath
-    val managedDependencies = basicScalaPaths.managedDependencyPath
+    	val entries = getJavaPaths ++ getScalaPaths ++ getProjectPath ++ getSbtJarForSbtProject ++
+	      			  getDependencyEntries(dependencies) ++ getDependencyEntries(managedDependencies) ++
+	      			  List(ClasspathEntry(Container, scalaContainer),
+	      			  ClasspathEntry(Container, javaContainer),
+	      			  ClasspathEntry(Output, project.asInstanceOf[MavenStyleScalaPaths].mainCompilePath))
 
-    val scalaContainer = "ch.epfl.lamp.sdt.launching.SCALA_CONTAINER"
-    val javaContainer = "org.eclipse.jdt.launching.JRE_CONTAINER"
+	    lazy val classpathContent = """<?xml version="1.0" encoding="UTF-8" ?>""" +
+	    	"\n<classpath>" +
+	    	("" /: entries)(_ + _.mkString("\n")) +
+	    	"\n</classpath>"
+	    createOrReplaceWith(classpathContent)
+  	}
 
-    val entries = getJavaPaths ++ getScalaPaths ++ getProjectPath ++ getSbtJarForSbtProject ++
-	      getDependencyEntries(dependencies) ++ getDependencyEntries(managedDependencies) ++
-	      List(ClasspathEntry(Container, scalaContainer),
-		   ClasspathEntry(Container, javaContainer),
-		   ClasspathEntry(Output, project.asInstanceOf[MavenStyleScalaPaths].mainCompilePath))
+    /**
+     * @return <code>List[ClasspathEntry]</code> with entries for the project build directory and the project plugin directory
+     */
+  	def getProjectPath: List[ClasspathEntry] = {
+	    val plugin = project.asInstanceOf[SbtEclipsifyPlugin]
+	    var entries = List[ClasspathEntry]()
+	    entries = if(plugin.includeProject.value && project.info.builderProjectPath.exists) {
+	    	ClasspathEntry(Source, project.info.builderProjectPath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
+	    } else entries
+	    if(plugin.includePlugin.value && project.info.pluginsPath.exists) {
+	    	ClasspathEntry(Source, project.info.pluginsPath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
+	    } else entries
+	}
 
-    lazy val classpathContent =
-      """<?xml version="1.0" encoding="UTF-8" ?>""" +
-      "\n<classpath>" +
-      ("" /: entries)(_ + _.mkString("\n")) +
-      "\n</classpath>"
-    createOrReplaceWith(classpathContent)
-  }
+    /**
+     * @return <code>List[ClasspathEntry]</code> with entries for the main source and main test source path.
+     */
+	def getScalaPaths: List[ClasspathEntry] = {
+	    val paths = project.asInstanceOf[MavenStyleScalaPaths]
+	    var entries = List[ClasspathEntry]()
+	    entries = if(paths.mainScalaSourcePath.exists) {
+	    	ClasspathEntry(Source, paths.mainScalaSourcePath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
+	    } else entries
+	    if(paths.testScalaSourcePath.exists) {
+	    	ClasspathEntry(Source, paths.testScalaSourcePath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
+	    } else entries
+	}
 
-  def getProjectPath: List[ClasspathEntry] = {
-    val plugin = project.asInstanceOf[SbtEclipsifyPlugin]
-    var entries = List[ClasspathEntry]()
-    entries = if(plugin.includeProject.value && project.info.builderProjectPath.exists) {
-    	ClasspathEntry(Source, project.info.builderProjectPath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
-    } else entries
-    if(plugin.includePlugin.value && project.info.pluginsPath.exists) {
-      ClasspathEntry(Source, project.info.pluginsPath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
-    } else entries
-  }
+    /**
+     * @return <code>List[ClasspathEntry]</code> for main java source and main java test source path
+     */
+	def getJavaPaths: List[ClasspathEntry] = {
+	    val paths = project.asInstanceOf[MavenStyleScalaPaths]
+	    var entries = List[ClasspathEntry]()
+	    entries = if (paths.testJavaSourcePath.exists) {
+	    	ClasspathEntry(Source, paths.testJavaSourcePath, FilterChain(IncludeFilter("**/*.java"))) :: entries
+	    } else entries
 
-  def getScalaPaths: List[ClasspathEntry] = {
-    val paths = project.asInstanceOf[MavenStyleScalaPaths]
-    var entries = List[ClasspathEntry]()
-    entries = if(paths.mainScalaSourcePath.exists) {
-    	ClasspathEntry(Source, paths.mainScalaSourcePath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
-    } else entries
-    if(paths.testScalaSourcePath.exists) {
-    	ClasspathEntry(Source, paths.testScalaSourcePath, FilterChain(IncludeFilter("**/*.scala"))) :: entries
-    } else entries
-  }
+	    if (paths.mainJavaSourcePath.exists) {
+	    	ClasspathEntry(Source, paths.mainJavaSourcePath, FilterChain(IncludeFilter("**/*.java"))) :: entries
+	    } else entries
+	}
 
-  def getJavaPaths: List[ClasspathEntry] = {
-    val paths = project.asInstanceOf[MavenStyleScalaPaths]
-    var entries = List[ClasspathEntry]()
-    entries = if (paths.testJavaSourcePath.exists) {
-      ClasspathEntry(Source, paths.testJavaSourcePath, FilterChain(IncludeFilter("**/*.java"))) :: entries
-    } else entries
 
-    if (paths.mainJavaSourcePath.exists) {
-      ClasspathEntry(Source, paths.mainJavaSourcePath, FilterChain(IncludeFilter("**/*.java"))) :: entries
-    } else entries
-  }
-
-  def getSbtJarForSbtProject: List[ClasspathEntry] = {
-    val scalaVersion = project.scalaVersion.get.get
-    val sbtVersion = project.sbtVersion.get.get
-    // TODO how to handle cross builds?
-    val sbtLibPath = project.info.projectPath / "project" / "boot" / ("scala-" + scalaVersion) / ("sbt-" + sbtVersion) / ("sbt_" + scalaVersion + "-" + sbtVersion + ".jar")
-    val plugin = project.asInstanceOf[SbtEclipsifyPlugin]
-    if(plugin.sbtDependency.value || plugin.includeProject.value || plugin.includePlugin.value)
-    	List(ClasspathEntry(Library, sbtLibPath)) else Nil
-  }
+	/**
+     * @return <code>List[ClasspathEntry]</code> for sbt jar
+     */
+	def getSbtJarForSbtProject: List[ClasspathEntry] = {
+	    val scalaVersion = project.scalaVersion.get.get
+	    val sbtVersion = project.sbtVersion.get.get
+	    // TODO how to handle cross builds?
+	    val sbtLibPath = project.info.projectPath / "project" / "boot" / ("scala-" + scalaVersion) / ("sbt-" + sbtVersion) / ("sbt_" + scalaVersion + "-" + sbtVersion + ".jar")
+	    val plugin = project.asInstanceOf[SbtEclipsifyPlugin]
+	    if(plugin.sbtDependency.value || plugin.includeProject.value || plugin.includePlugin.value)
+	    	List(ClasspathEntry(Library, sbtLibPath))
+	    else Nil
+	}
 }
 
+/**
+ * Factory to for creating ClasspathFile instances
+ */
 object ClasspathFile {
   def apply(project: Project, log: Logger) = new ClasspathFile(project, log)
 }
