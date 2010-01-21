8fc69285b64a802587ae21a33a6526179823a4f6
Stefan Langer <mailtolanger@googlemail.com>
2010-01-21 17:54:06 +0100

    Upgraded to version 0.4.2 and readded sbt plugin
    Readded the dependency to the sbt-eclipsify plugin in version 0.4.1 as this is the official public release

--- project/build/SbtEclipsifyProject.scala
+++ project/build/SbtEclipsifyProject.scala
@@ -1,13 +1,44 @@
+/**
+ * Copyright (c) 2010, Stefan Langer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Element34 nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS ROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 import sbt._
 import de.element34.sbteclipsify._
 
-class SbtEclipsifyPluginProject(info: ProjectInfo) extends PluginProject(info) { // with SbtEclipsifyPlugin {
+class SbtEclipsifyPluginProject(info: ProjectInfo) extends PluginProject(info) with SbtEclipsifyPlugin {
 	override def compileOptions =  super.compileOptions ++ (Unchecked :: Deprecation :: Nil)
   	override def mainResources = super.mainResources +++ "NOTICE" +++ "LICENSE" +++ (path("licenses") * "*")
 
   	lazy val scalaTest = "org.scalatest" % "scalatest" % "1.0"
 
   	override def managedStyle = ManagedStyle.Maven
-  	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-  	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+  	val credPath = Path.userHome / ".ivy2" / ".credentials"
+  	Credentials(credPath, log)
+
+  	val publishTo = "Sonatype Nexus Repository Manager" at "http://nexus.scala-tools.org/content/repositories/releases/"
 }
